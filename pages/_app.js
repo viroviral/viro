b7aec91c50fdea4b2cec2fe53586605d5a556323
Marco <marcobusiness1011@gmail.com>
2025-06-20 15:38:43 +0200

    Añadido Vercel Analytics en app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,11 @@
 import "@/styles/globals.css";
-import Script from "next/script"; // 👈 Importa Script de next/script
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      {/* Vercel Web Analytics */}
-      <Script
-        src="https://vercel.com/analytics/script.js"
-        strategy="lazyOnload"
-      />
-      
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
